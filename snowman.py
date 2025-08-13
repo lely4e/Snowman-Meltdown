30fffa54c601b23b340775d076455425843a6bfe
Olga Masterschool <lely4e@gmail.com>
2025-08-13 18:13:20 +0200

    Added a new function display_game_state, ASCII art (stage 0) and word displaying with underscores

--- snowman.py
+++ snowman.py
@@ -1,5 +1,35 @@
 import random
 
+# Snowman ASCII Art stages
+STAGES = [
+     # Stage 0: Full snowman
+     """
+      ___  
+     /___\\ 
+     (o o) 
+     ( : ) 
+     ( : ) 
+     """,
+     # Stage 1: Bottom part starts melting
+     """
+      ___  
+     /___\\ 
+     (o o) 
+     ( : ) 
+     """,
+     # Stage 2: Only the head remains
+     """
+      ___  
+     /___\\ 
+     (o o) 
+     """,
+     # Stage 3: Snowman completely melted
+     """
+      ___  
+     /___\\ 
+     """
+ ]
+
 # List of secret words
 WORDS = ["python", "git", "github", "snowman", "meltdown"]
 
@@ -11,13 +41,31 @@ def get_random_word():
 
 def play_game():
     secret_word = get_random_word()
-    print("Welcome to Snowman Meltdown!")
-    print("Secret word selected: " + secret_word)  # for testing, later remove this line
+    print("Secret word: " + secret_word)
+    mistakes = 0
+    guessed_letters = []
 
-    # TODO: Build your game loop here.
+    print("Welcome to Snowman Meltdown!")
+    display_game_state(mistakes, secret_word, guessed_letters)
+
     # For now, simply prompt the user once:
     guess = input("Guess a letter: ").lower()
     print("You guessed:", guess)
+
+
+def display_game_state(mistakes, secret_word, guessed_letters):
+    """Displays the game state."""
+    print(STAGES[mistakes])
+    guessed_word = ""
+
+    for letter in secret_word:
+        if letter in guessed_letters:
+            guessed_word += letter + " "
+        else:
+            guessed_word += "_ "
+
+    print("Word: ", guessed_word )
+    print(STAGES[0])
 
 
 if __name__ == "__main__":
