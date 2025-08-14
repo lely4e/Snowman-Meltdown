08789e5247e0eb62039caa9267666d9ac07adc74
Olga Masterschool <lely4e@gmail.com>
2025-08-14 14:48:32 +0200

    Clarifying the main function and moving the rest of the files to the game_logic

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+from game_logic import play_game
 
-# List of secret words
-WORDS = ["python", "git", "github", "snowman", "meltdown"]
+
+def main():
+    play_game()
 
 
 if __name__ == "__main__":
-    play_game()
+    main()
