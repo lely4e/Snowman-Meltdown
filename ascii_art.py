60c851e0229ba23f054284ae8884bb39c9387fee
Olga Masterschool <lely4e@gmail.com>
2025-08-14 16:44:44 +0200

    Added a new ascii art with smoother transition

--- ascii_art.py
+++ ascii_art.py
@@ -2,28 +2,56 @@
 STAGES = [
      # Stage 0: Full snowman
      """
-      ___  
-     /___\\ 
-     (o o) 
-     ( : ) 
-     ( : ) 
+       ___  
+      /___\\ 
+      (o o) 
+     <( : )>
+      ( : ) 
      """,
      # Stage 1: Bottom part starts melting
      """
-      ___  
-     /___\\ 
-     (o o) 
-     ( : ) 
+       ___  
+      /___\\ 
+      (o o) 
+     /( : )> 
+      __ : _ 
      """,
-     # Stage 2: Only the head remains
+     # Stage 2: Bottom part still melting with hands
      """
-      ___  
-     /___\\ 
-     (o o) 
+       ___  
+      /___\\ 
+      (o o) 
+     /( : )\  
      """,
-     # Stage 3: Snowman completely melted
+     # Stage 3: Bottom part almost melted
      """
-      ___  
-     /___\\ 
+       ___  
+      /___\\ 
+      (o o) 
+      _  : )_
+     """,
+     # Stage 4: The head remains with hat
+     """
+       ___  
+      /___\\ 
+      (o o)__
+     """,
+     # Stage 5: Hat and face sliding
+     """
+        ___  
+       /___\\ 
+      (   o)>
+     """,
+     # Stage 6: Only the hat remains
+     """
+         ___  
+      __/___\\ __
+     """,
+     # Stage 7: Snowman completely melted to snow
+     """
+         * * *
+        * * * *
+         * * *
      """
  ]
+
